ce198584954e37a47f5e0ba4eb75aa46a56446aa
Rextin <fungtinrex04@gmail.com>
2022-09-10 11:48:25 +0800

    10/09 img opacity

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,17 @@
 $(function () {
   load();
 
+  $(".meun ul li").hover(
+    function () {
+      //mousein li - silbings = li borther opacity to 0.1
+      $(this).children().siblings(".moon_icon").stop().fadeTo(400, 1);
+    },
+    function () {
+      //mouseOut li - silbings  opacity back to 1
+      $(this).children().siblings(".moon_icon").stop().fadeTo(400, 0.1);
+    },
+  );
+
   //1) mouseover the li
   $(".meun ul li ").click(function () {
     //2)get the li index
@@ -12,17 +23,6 @@ $(function () {
     $(this).children("a").hide();
     $(this).siblings().children("a").show();
   });
-
-  $(".meun ul li").hover(
-    function () {
-      //mousein li - silbings = li borther opacity to 0.1
-      $(this).children().siblings(".moon_icon").stop().fadeTo(400, 0.1);
-    },
-    function () {
-      //mouseOut li - silbings  opacity back to 1
-      $(this).children().siblings(".moon_icon").stop().fadeTo(400, 1);
-    },
-  );
 
   $(".question li").hover(function () {
     $(this).children("h4").stop().fadeToggle(400);
